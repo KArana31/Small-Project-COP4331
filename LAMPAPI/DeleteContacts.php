415491f1127baf8df5d7b63465e42fec5d685b65
ks-cs <74277124+ks-cs@users.noreply.github.com>
2022-06-04 12:32:33 -0400

    Add files via upload

---
+++ LAMPAPI/DeleteContacts.php
@@ -0,0 +1,45 @@
+<?php?
+
+	$inData = getRequestInfo();
+	
+	$conn = new mysqli("localhost", "TheBeast", "WeLoveCOP4331", "COP4331");
+	if ($conn->connect_error) 
+	{
+		returnWithError( $conn->connect_error );
+	} 
+	else
+	{
+		try{
+			$stmt = $conn->prepare("DELETE FROM Contacts WHERE ID = ? AND UserID = ?");
+			$stmt->bind_param("ss", $inData["ID"], $inData["UserID"]);
+			
+			
+			
+			$stmt->execute();
+			$stmt->close();
+			$conn->close();
+			returnWithError("");
+		
+		}
+		catch(Exception $ex){
+			returnWithError( $ex->getMessage );
+		}
+	}
+
+	function getRequestInfo()
+	{
+		return json_decode(file_get_contents('php://input'), true);
+	}
+	
+	function sendResultInfoAsJson( $obj )
+	{
+		header('Content-type: application/json');
+		echo $obj;
+	}
+	
+	function returnWithError( $err )
+	{
+		$retValue = '{"error":"' . $err . '"}';
+		sendResultInfoAsJson( $retValue );
+	}
+>
